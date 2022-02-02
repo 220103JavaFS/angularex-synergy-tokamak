66b98783ad1d5164c717bff0324bac50ba430741
rogerjarad <rogerjaradgaulke@gmail.com>
2022-02-02 12:03:43 -0800

    R

---
+++ src/app/components/rogers/rogers.component.spc.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RogersComponent } from './rogers.component';
+
+describe('RogersComponent', () => {
+    let component: RogersComponent;
+    let fixture: ComponentFixture<RogersComponent>;
+  
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [ RogersComponent ]
+      })
+      .compileComponents();
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(RogersComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+  
