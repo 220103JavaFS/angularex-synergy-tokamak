6c4f3b48c42550cfd335ae7cb381792e937ee081
JBOhge <ohgejb@gmail.com>
2022-02-02 14:08:04 -0600

    update joel component

--- src/app/components/joel-page/joel-page.component.ts
+++ src/app/components/joel-page/joel-page.component.ts
@@ -3,13 +3,62 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-joel-page',
   templateUrl: './joel-page.component.html',
-  styleUrls: ['./joel-page.component.css']
+  styleUrls: ['./joel-page.component.css'],
 })
 export class JoelPageComponent implements OnInit {
+  pokemonList: any[] = [];
+  pokemon: any = '';
+  url: string = 'https://pokeapi.co/api/v2/pokemon/249';
 
-  constructor() { }
+  async getPokemon() {
+    var response = await fetch('https://pokeapi.co/api/v2/pokemon/249', {
+      method: 'GET',
+    });
 
-  ngOnInit(): void {
+    var pokemon = await response.json();
+    this.pokemonList.push(pokemon);
+
+    response = await fetch('https://pokeapi.co/api/v2/pokemon/6', {
+      method: 'GET',
+    });
+
+    pokemon = await response.json();
+    this.pokemonList.push(pokemon);
+
+    response = await fetch('https://pokeapi.co/api/v2/pokemon/384', {
+      method: 'GET',
+    });
+
+    pokemon = await response.json();
+    this.pokemonList.push(pokemon);
+
+    response = await fetch('https://pokeapi.co/api/v2/pokemon/131', {
+      method: 'GET',
+    });
+
+    pokemon = await response.json();
+    this.pokemonList.push(pokemon);
+
+    response = await fetch('https://pokeapi.co/api/v2/pokemon/373', {
+      method: 'GET',
+    });
+
+    pokemon = await response.json();
+    this.pokemonList.push(pokemon);
+
+    response = await fetch('https://pokeapi.co/api/v2/pokemon/130', {
+      method: 'GET',
+    });
+
+    pokemon = await response.json();
+    this.pokemonList.push(pokemon);
   }
 
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getPokemon();
+    console.log(this.pokemonList);
+  }
 }
