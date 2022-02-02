147fd0ca00742f54def87a17470de877f79a5451
merry-degaga <merry.degaga@revature.net>
2022-02-02 15:17:14 -0500

    added navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BERHEProfileComponent } from './components/berhe-profile/berhe-profile.component';
+import { EdwinComponent } from './components/edwin/edwin.component';
 import { JoelPageComponent } from './components/joel-page/joel-page.component';
+import { MattPageComponent } from './components/matt-page/matt-page.component';
 import { MerryComponent } from './components/merry/merry.component';
+import { RogersComponent } from './components/rogers/rogers.component';
 
 const routes: Routes = [{
   path: "joel",
   component: JoelPageComponent
 },
 {path:"merry",
-component:MerryComponent}];
+component:MerryComponent},
+{path:"matt",
+component:MattPageComponent},
+{path:"roger",
+component:RogersComponent}, 
+{path:"edwin", 
+component:EdwinComponent},
+{path:"berhe", 
+component:BERHEProfileComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/edwin/edwin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edwin',
+  templateUrl: './edwin.component.html',
+  styleUrls: ['./edwin.component.css']
+})
+export class EdwinComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
